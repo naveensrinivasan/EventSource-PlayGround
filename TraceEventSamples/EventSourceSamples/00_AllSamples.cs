846ec614c19e817958e85a8fbddb2106c970c7ca
Naveen <naveensrinivasan@yahoo.com>
2015-05-26 18:03:30 -0400

    Initial Commit
    Initial Commit

---
+++ TraceEventSamples/EventSourceSamples/00_AllSamples.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace EventSourceSamples
+{
+    public static class AllSamples
+    {
+        /// <summary>
+        /// The samples are 'console based' in that they spew text to an output stream.   By default this is
+        /// the Console, but you can redirect it elsewhere by overriding this static variable.  
+        /// </summary>
+        public static TextWriter Out = Console.Out;
+
+        /// <summary>
+        /// This is the main entry point for all the samples.   It runs them sequentially
+        /// </summary>
+        public static void Run()
+        {
+                CustomizedEventSourceDemo.Run(); 
+
+        }
+    }
+}
---
+++ TraceEventSamples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventSourceSamples;
+namespace TraceEventSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AllSamples.Run();
+        }
+    }
+}
